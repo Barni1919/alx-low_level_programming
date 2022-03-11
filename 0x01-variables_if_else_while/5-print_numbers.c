f473bc2063e326bcc4cc234388792c91e82e781e
root <barni1919s@gmail.com>
2022-03-11 04:51:30 -0800

    0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* 4th project */
+
+/**
+ * main - Entry
+ *
+ * Return: always zero 0 (success)
+ */
+int main(void)
+{
+
+	int x;
+
+	while ( x <= 10)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
