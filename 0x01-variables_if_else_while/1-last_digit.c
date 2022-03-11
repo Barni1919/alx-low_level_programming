c6a0470b1ab2c92b35dcfe0e8854f025007be6cd
root <barni1919s@gmail.com>
2022-03-11 01:59:00 -0800

    4th

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -10,20 +9,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		if (n == 0)
-		{
-			printf("%d is zero\n" , n);
-		}
-		else
-		{
-			if(n < 0)
-			{
-				printf("%d is negative\n", n);
-			}
-			else
-			{
-				printf("%d is positive\n", n);
-			}
-		}
+		
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* alphabet project */
+
+int main(void)
+{
+	char x;
+
+	for(x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+		putchar('\n');
+	}
+	return (0);
+}
