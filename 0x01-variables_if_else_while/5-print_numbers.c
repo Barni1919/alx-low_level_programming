a0d48bee556d3ecaa4c6ac6b24bfab73bf8cb02d
root <barni1919s@gmail.com>
2022-03-11 05:15:12 -0800

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* 4th project */
 
@@ -10,12 +11,11 @@
 int main(void)
 {
 
-	int x = 0;
+	int x;
 
-	while (x < 10)
+	for (x = 0; x < 10; x++)
 	{
-		putchar(x);
-		x++;
+		printf("%d", x);
 	}
 	putchar('\n');
 
