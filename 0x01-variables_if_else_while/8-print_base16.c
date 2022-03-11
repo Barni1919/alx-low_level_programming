60c073838059443a26496f0422b4c38b6a4b8e68
root <barni1919s@gmail.com>
2022-03-11 13:06:59 -0800

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* No 3 */
+
+/**
+ * main - message goes here
+ *
+ * Return: the code returns 0 and chars (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '48'; x <= '58'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'a'; x <= 'g'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
