2c158aaf1799445e3f046ac37a3bed5fe46dc562
root <barni1919s@gmail.com>
2022-03-11 03:58:24 -0800

    f

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,15 +2,20 @@
 
 /* No 3 */
 
+/**
+ * main - message goes here
+ *
+ * the code returns 0 and chars
+ */
 int main(void)
 {
 	char x;
 
-	for(x = 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
 	}
-	for(x = 'A'; x <= 'Z'; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 	{
 		putchar(x);
 	}
