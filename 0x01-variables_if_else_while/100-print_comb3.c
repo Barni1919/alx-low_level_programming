9d1a46c4c0e5d41622a41228c3634c9be9112bb8
root <barni1919s@gmail.com>
2022-03-11 23:51:56 -0800

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 4th project */
+
+/**
+ * main - Entry
+ *
+ * Return: always zero 0 (success)
+ */
+int main(void)
+{
+
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (int y = x+1; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			if (((x * 10) + y) < 89)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
